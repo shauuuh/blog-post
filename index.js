8594cdcf057cf28c2a4fd3f86f7707424fdfdd9c
shauuuh <shauryhsw@gmail.com>
2024-08-19 20:26:07 -0600

    adding API images

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express, { text } from "express";
 import bodyParser from "body-parser";
+import axios from "axios";
 
 const app = express();
 const port = 3000;
+const API_URL = "https://picsum.photos/200";
 
 app.use(bodyParser.urlencoded({ extended:true }));
 
@@ -15,17 +17,24 @@ app.get("/",(req,res) => {
   console.log(items);
 });
 
-app.post("/submit",(req,res) => {
-  //const textPost = req.body["post-text"];
-  const newItem = {
-    id : items.length + 1,
-    title : req.body["title-post"],
-    text : req.body["post-text"],
-  };
-  //console.log(newItem);
-  items.push(newItem);
-  res.redirect('/');
-  //res.render("index.ejs", newItem);
+app.post("/submit",async(req,res) => {
+  try {
+    const result = await axios.get(API_URL);
+    //const textPost = req.body["post-text"];
+    const newItem = {
+      id : items.length + 1,
+      title : req.body["title-post"],
+      text : req.body["post-text"],
+      image : JSON.stringify(result.data),
+    };
+    //console.log(newItem);
+    items.push(newItem);
+    res.redirect('/');
+    //res.render("index.ejs", newItem);
+  } catch (error) {
+    
+  }
+  
 });
 
 app.post("/edit/:id",(req,res)=>{
