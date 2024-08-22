5305133c35b6a606f9881d94d80eaea70c8ff442
shauuuh <shauryhsw@gmail.com>
2024-08-21 20:03:00 -0600

    pg db added

--- index.js
+++ index.js
@@ -1,49 +1,68 @@
-import express, { text } from "express";
+import express from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
 const API_URL = "https://picsum.photos/200";
 
-app.use(bodyParser.urlencoded({ extended:true }));
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "blog",
+  password: "shau2799",
+  port: 5432,
+});
 
+db.connect();
+
+app.use(bodyParser.urlencoded({ extended:true }));
 app.use(express.static("public"));
 
 let items = [];
 
-app.get("/",(req,res) => {
-  res.render("index.ejs",{ items });
-  console.log(items);
+app.get("/", async(req,res) => {
+  try {
+    const response = await db.query("SELECT * FROM posts ORDER BY id DESC");
+    items = response.rows;
+    res.render("index.ejs",{ items });
+    console.log(items);
+  } catch (error) {
+    console.log(err);
+  }
 });
 
-app.post("/submit",async(req,res) => {
+app.post("/submit", async(req,res) => {
   try {
-    const result = await axios.get(API_URL);
-    //const textPost = req.body["post-text"];
+    const result = await axios.get(API_URL, {responseType: 'stream'});
+    const title = req.body["title-post"];
+    const text = req.body["post-text"];
+    //const image = result.data.responseUrl;
+    /*
     const newItem = {
       id : items.length + 1,
       title : req.body["title-post"],
       text : req.body["post-text"],
-      image : JSON.stringify(result.data),
-    };
-    //console.log(newItem);
-    items.push(newItem);
+      image : result.data.responseUrl,
+    };*/
+    await db.query("INSERT INTO posts (title,txt_content,url_image) VALUES ($1,$2,$3)",[title,text,result.data.responseUrl]);
+    //items.push(newItem);
     res.redirect('/');
     //res.render("index.ejs", newItem);
   } catch (error) {
-    
+    console.log(error);
   }
-  
 });
 
-app.post("/edit/:id",(req,res)=>{
+app.post("/edit/:id", async(req,res)=>{
   const id = parseInt(req.params.id);
-  const item = items.find(item => item.id === id);
+  const item = items.findIndex(item => item.id === id);
+  const response = await db.query("SELECT * FROM posts WHERE id = $1",[id]);
   const data = {
-    id_item : item.id,
-    title : item.title,
-    text : item.text,
+    id_item : response.id,
+    title : response.title,
+    text : response.txt_content,
   }  
   res.render('edit.ejs',data)
 });
